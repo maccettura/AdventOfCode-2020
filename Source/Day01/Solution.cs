e6fbdf1c8f99f0910ca717854da74e29ff8d4326
Michelangelo Accettura III <michelangelo.accettura@gmail.com>
2020-12-01 01:03:11 -0500

    Adding Day 1

--- Source/Day01/Solution.cs
+++ Source/Day01/Solution.cs
@@ -1,21 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Day01
 {
     public class Solution : BaseSolution
     {
-        public Solution() : base(1, "")
+        public Solution() : base(1, "Report Repair")
         {
         }
 
         public override string GetPart1Answer()
         {
+            var expenseHashSet = GetInputHashSet();
+
+            foreach (var num in expenseHashSet)
+            {
+                int result = 2020 - num;
+                if (expenseHashSet.Contains(result))
+                {
+                    return $"{num * result}";
+                }
+            }
             return string.Empty;
         }
 
         public override string GetPart2Answer()
         {
+            var expenseHashSet = GetInputHashSet();
+
+            foreach (var num1 in expenseHashSet)
+            {
+                foreach (var num2 in expenseHashSet)
+                {
+                    int result = 2020 - num1 - num2;
+                    if (expenseHashSet.Contains(result))
+                    {
+                        return $"{num1 * num2 * result}";
+                    }
+                }
+            }
             return string.Empty;
+        }
+
+        private HashSet<int> GetInputHashSet()
+        {
+            var input = GetResourceString();
+
+            return new HashSet<int>(
+                input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x)));
         }
     }
 }
