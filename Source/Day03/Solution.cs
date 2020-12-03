58a3f110bc7dd4ac01b5c7ffe721d65bcdd54c33
Michelangelo Accettura III <michelangelo.accettura@gmail.com>
2020-12-03 14:43:15 -0500

    Adding Day 3

--- Source/Day03/Solution.cs
+++ Source/Day03/Solution.cs
@@ -1,21 +1,56 @@
 ﻿using System;
+using System.Linq;
 
 namespace AdventOfCode.Day03
 {
     public class Solution : BaseSolution
     {
-        public Solution() : base(3, "")
+        public Solution() : base(3, "Toboggan Trajectory")
         {
         }
 
         public override string GetPart1Answer()
         {
-            return string.Empty;
+            var lines = GetResourceString().Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            return FindTeeCount(lines, 1, 3).ToString();
         }
 
         public override string GetPart2Answer()
         {
-            return string.Empty;
+            var lines = GetResourceString().Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            var slopes = new (int slopeDown, int slopeRight)[] 
+            {
+                (1, 1),
+                (1, 3),
+                (1, 5),
+                (1, 7),
+                (2, 1),
+            };
+
+            return slopes.Select(x => FindTeeCount(lines, x.slopeDown, x.slopeRight)).Aggregate((a, x) => a * x).ToString();
+        }
+
+        private static int FindTeeCount(string[] lines, int slopeDown, int slopeRight)
+        {
+            int treeCount = 0;
+            int index = 0;
+
+            for (int i = 0; i < lines.Length; i += slopeDown)
+            {
+                if (index == 0)
+                {
+                    index += slopeRight;
+                    continue;
+                }
+
+                if (lines[i][index >= 31 ? index % 31 : index] == '#')
+                {
+                    treeCount++;
+                }
+                index += slopeRight;
+            }
+            return treeCount;
         }
     }
 }
