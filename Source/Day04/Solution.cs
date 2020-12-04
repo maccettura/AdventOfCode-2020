550cf3e9d2694017cae9e61161c6853a95bf176a
Michelangelo Accettura III <michelangelo.accettura@gmail.com>
2020-12-04 17:47:45 -0500

    Day 4 regex fixes and removal of string builder

--- Source/Day04/Solution.cs
+++ Source/Day04/Solution.cs
@@ -13,10 +13,9 @@ namespace AdventOfCode.Day04
 
         public override string GetPart1Answer()
         {
-            var lines = GetResourceString().Split(Environment.NewLine);
+            var lines = GetResourceString().Split(Environment.NewLine + Environment.NewLine);
 
             string[] keys = new[] { "byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:" };
-
             var validCount = GetValidCount(lines, Validate);
 
             return validCount.ToString();
@@ -26,20 +25,20 @@ namespace AdventOfCode.Day04
                 return keys.All(passport.Contains);
             }    
         }
-               
+
         public override string GetPart2Answer()
         {
-            var lines = GetResourceString().Split(Environment.NewLine);
+            var lines = GetResourceString().Split(Environment.NewLine + Environment.NewLine);
 
             var regexes = new[]
             {
                 new Regex(@"byr:(19[2-9]\d|200[0-2])"),
                 new Regex(@"iyr:(201\d|2020)"),
                 new Regex(@"eyr:(202\d|2030)"),
-                new Regex(@"hgt:(1[5-8]\dcm|19[0-3]cm|[5-6]\din|7[0-6]in)"),
+                new Regex(@"hgt:(1[5-8]\dcm|19[0-3]cm|59in|6\din|7[0-6]in)"),
                 new Regex(@"hcl:#[0-9a-f]{6}"),
                 new Regex(@"ecl:(amb|blu|brn|gry|grn|hzl|oth)"),
-                new Regex(@"pid:[\d]{9}")
+                new Regex(@"pid:[\d]{9}($|\s)")
             };
 
             var validCount = GetValidCount(lines, Validate);
@@ -48,36 +47,20 @@ namespace AdventOfCode.Day04
 
             bool Validate(string passport)
             {
-                foreach(var regex in regexes)
-                {
-                    if(!regex.Match(passport).Success)
-                    {
-                        return false;
-                    }
-                }
-                return true;
+                return regexes.All(x => x.Match(passport).Success);
             }
         }
 
-        private static int GetValidCount(string[] lines, Func<string, bool> validator)
+        private static int GetValidCount(string[] passports, Func<string, bool> validator)
         {
             int validCount = 0;
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < lines.Length; i++)
+            foreach(var passport in passports)
             {
-                sb.Append($"{lines[i]} ");
-                if (string.IsNullOrWhiteSpace(lines[i]))
+                if (validator(passport))
                 {
-                    if (validator(sb.ToString()))
-                    {
-                        validCount++;
-                    }
-                    sb.Clear();
-                    continue;
+                    validCount++;
                 }
             }
-
             return validCount;
         }
     }
