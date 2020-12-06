22b1e12daa165a17483f8017b66944aac5f70db7
Michelangelo Accettura III <michelangelo.accettura@gmail.com>
2020-12-06 00:10:05 -0500

    Adding Day 6

--- Source/Day06/Solution.cs
+++ Source/Day06/Solution.cs
@@ -1,21 +1,39 @@
 ﻿using System;
+using System.Linq;
 
 namespace AdventOfCode.Day06
 {
     public class Solution : BaseSolution
     {
-        public Solution() : base(6, "")
+        public Solution() : base(6, "Custom Customs")
         {
         }
 
         public override string GetPart1Answer()
         {
-            return string.Empty;
+            var groups = GetResourceString().Split(Environment.NewLine + Environment.NewLine);
+
+            int total = 0;
+            foreach(var group in groups)
+            {
+                total += group.Where(c => char.IsLetter(c)).Distinct().Count();
+            }
+
+            return total.ToString();
         }
 
         public override string GetPart2Answer()
         {
-            return string.Empty;
+            var groups = GetResourceString().Split(Environment.NewLine + Environment.NewLine);
+
+            int total = 0;
+            foreach (var group in groups)
+            {
+                var personCount = group.Split(Environment.NewLine).Length;
+                total += group.Where(c => char.IsLetter(c)).GroupBy(x => x).Where(x => x.Count() >= personCount).Count();
+            }
+
+            return total.ToString();
         }
     }
 }
